d31d2d7c8941e1d8de7e6d98f497b3f1c8b4a5b6
Andrei <161997870+andreicuruja@users.noreply.github.com>
2025-08-11 17:15:55 -0300

    Add files via upload

---
+++ janelauntitled/janelauntitled.py
@@ -0,0 +1,16 @@
+#importar 
+from PyQt5 import uic,QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+
+def exibir_mensagem():
+    dado_lido = janela.lineEdit.text()
+    print(dado_lido)
+    janela.lineEdit.setText("")
+    QMessageBox.about(janela,"alerta", dado_lido)
+
+#programa principal
+app=QtWidgets.QApplication([])
+janela=uic.loadUi("janelauntitled.ui")
+janela.pushButton.clicked.connect(exibir_mensagem)
+janela.show()
+app.exec()

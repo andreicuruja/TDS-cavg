baac544c72d0fd418020023f3e5ab4046e815b8c
Andrei <161997870+andreicuruja@users.noreply.github.com>
2025-08-14 16:47:16 -0300

    Add files via upload
    update com o exercicio 2 do dia 14/08/2025

--- janelauntitled/janelauntitled.py
+++ janelauntitled/janelauntitled.py
@@ -8,9 +8,43 @@ def exibir_mensagem():
     janela.lineEdit.setText("")
     QMessageBox.about(janela,"alerta", dado_lido)
 
+#exercicio dois:
+def exibir_mensagem():
+    dado_lido = janela.lineEdit.text()
+    print(dado_lido)
+    janela.lineEdit.setText("")
+    if dado_lido=="":
+        QMessageBox.about(janela,"Nome:", "Nenhum nome foi digitado")
+    else:
+        QMessageBox.about(janela,"Nome:", "Olá, "+dado_lido)
+
+def fechar_tela():
+    result = QMessageBox.question(janela, "Saindo do sistema", "Deseja mesmo sair do sistema?", QMessageBox.Yes, QMessageBox.No)
+    if result == QMessageBox.Yes:
+        janela.close()
+
+def listar_dados():
+    dado_lido = janela.lineEdit.text()
+    janela.lista.addItem(dado_lido)
+
+def deletar():
+    janela.lista.clear()
+
+def contar_itens():
+    valor = janela.lista.count()
+    # janela.lbconta.setText("Qtde de itens: " + str(valor))
+
+def ordenar():
+    janela.lista.sortItems()
+    
 #programa principal
 app=QtWidgets.QApplication([])
 janela=uic.loadUi("janelauntitled.ui")
 janela.PushButton.clicked.connect(exibir_mensagem)
 janela.show()
+janela.btfechar.clicked.connect(fechar_tela)
+janela.btadicionar.clicked.connect(listar_dados)
+janela.btexcluir.clicked.connect(deletar)
+janela.btadicionar.clicked.connect(contar_itens)
+janela.btordenar.clicked.connect(ordenar)
 app.exec()

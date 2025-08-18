a5071e488365ebd5961e190873baaea3fee8732c
Andrei <161997870+andreicuruja@users.noreply.github.com>
2025-08-18 17:14:40 -0300

    Add files via upload
    atv 3 eu acho 18/08/2025

--- janelauntitled/janelauntitled.py
+++ janelauntitled/janelauntitled.py
@@ -24,19 +24,53 @@ def fechar_tela():
         janela.close()
 
 def listar_dados():
-    dado_lido = janela.lineEdit.text()
-    janela.lista.addItem(dado_lido)
+# dado_lido = janela.lineEdit.text()
+# janela.lista.addItem(dado_lido)
+#Nome
+    dado_lido = janela.leNome.text()
+#Confere Nome se n estiver vazio
+    if dado_lido=="":
+        QMessageBox.about(janela,"Nome:", "Nenhum nome foi digitado")
+    else:
+#Idade
+        dado_lido2 = janela.leIdade.text()
+#Confere Idade se n estiver vazio
+    if dado_lido2=="":
+        QMessageBox.about(janela,"Idade:", "Idade Vazia")
+    else:
+#Sexo
+        if janela.rb1.isChecked():
+            dado_lido3= 'M'
+        elif janela.rb2.isChecked():
+            dado_lido3= 'F'
+        else:
+            dado_lido3= 'ND'
+#Escolaridade
+    dado_lido4=janela.cbnivel.currentText()
+#Juntar tudo
+    dados = "Nome: "+dado_lido+"\nIdade: "+dado_lido2+"\nSexo:"+dado_lido3+"\nEscolaridade: "+dado_lido4
+#Limpar
+    janela.leNome.setText("")
+    janela.leIdade.setText("")
+#Adicionar a Lista msm
+    janela.lista.addItem(dados)
+#Contador
+    contar_itens()
 
 def deletar():
     janela.lista.clear()
+    result = QMessageBox.question(janela, "Excluir Itens", "Deseja mesmo excluir todos os itens da Lista?", QMessageBox.Yes, QMessageBox.No)
+    if result == QMessageBox.Yes:
+        janela.lista.clear()
+        contar_itens()
 
 def contar_itens():
     valor = janela.lista.count()
-    # janela.lbconta.setText("Qtde de itens: " + str(valor))
+    janela.leconta.setText("Nº de Itens: " + str(valor))
 
 def ordenar():
     janela.lista.sortItems()
-    
+
 #programa principal
 app=QtWidgets.QApplication([])
 janela=uic.loadUi("janelauntitled.ui")
